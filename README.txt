7e6be04b6b5716f53cb78130e69d2443b1a66eae
Beverly Lau <beverly.a.lau@gmail.com>
2017-05-02 15:14:36 -0700

    First commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Learning some Lua (from Seven More Languages in Seven Weeks).
+
+2017 May 2
+Beverly Lau
---
+++ functions.lua
@@ -0,0 +1,14 @@
+function call_twice(f)
+	ff = function(num)
+		return f(f(num))
+	end
+	return ff
+end
+
+function triple(n)
+	return n * 3
+end
+
+times_nine = call_twice(triple)
+
+print(tostring(times_nine(5)))
