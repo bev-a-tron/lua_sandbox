11d6077a1828cb8b666f5ede360163b1bf4cd4cf
Beverly Lau <beverly.a.lau@gmail.com>
2017-05-03 11:06:25 -0700

    Add examples for arguments, multiple return values, and tail calls

---
+++ args.lua
@@ -0,0 +1,8 @@
+function print_characters(friend, foe)
+    print('*Friend and foe*')
+    print(friend)
+    print(foe)
+end
+print_characters('Marcus', 'Belloq')
+print_characters('Marcus')
+print_characters('Marcus', 'Belloq', 'unused')
---
+++ multiple_return_values.lua
@@ -0,0 +1,15 @@
+function weapons()
+	return 'bullwhip', 'revolver'
+end
+
+w1 = weapons()
+print(w1)
+
+w1, w2 = weapons()
+print(w1)
+print(w2)
+
+w1, w2, w3 = weapons()
+print(w1)
+print(w2)
+print(w3)
