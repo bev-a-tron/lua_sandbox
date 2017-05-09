937cb6c9c2782f98f58eeb798855d23d3b7b2304
Beverly Lau <beverly.a.lau@gmail.com>
2017-05-09 11:48:28 -0700

    Instantiate objects

---
+++ day2/video_game.lua
@@ -0,0 +1,19 @@
+dietrich = {
+	name		= "Dietrich",
+	health		= 100,
+	take_hit	= function(self)
+						self.health	 = self.health - 10
+				  end
+}
+
+clone = {
+	name		= dietrich.name,
+	health		= dietrich.health,
+	take_hit	= dietrich.take_hit
+}
+
+print(clone.health)
+
+dietrich.take_hit(dietrich)
+print(dietrich.health)
+print(clone.health)
