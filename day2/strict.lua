c5f322b2833ccdf5b75559af8dd0245177a849e5
Beverly Lau <beverly.a.lau@gmail.com>
2017-05-09 11:38:21 -0700

    Raise error on duplicate key for writing

--- day2/strict.lua
+++ day2/strict.lua
@@ -1,6 +1,6 @@
-function strict_read(table, key)
-	local _private = {}
+local _private = {}
 
+function strict_read(table, key)
 	if _private[key] then
 		return _private[key]
 	else
@@ -8,14 +8,31 @@ function strict_read(table, key)
 	end
 end
 
+function strict_write(table, key, value)
+	if _private[key] then
+		error("Duplicate key: " .. key)
+	else
+		_private[key] = value
+	end
+end
+
+greek_numbers = {}
+mt = { __index = strict_read,
+	   __newindex = strict_write }
+
 greek_numbers = { ena = "one", 
 				  dyo = "two", 
 				  tria = "three" }
 
-mt = { __index = strict_read }
+setmetatable(greek_numbers, mt)
+print(greek_numbers.ena)
+print(greek_numbers.dyo)
+print(greek_numbers.tria)
+-- print(greek_numbers.lalal)
 
-setmetatable( greek_numbers, mt )
-print(greek_numbers['ena'])
-print(greek_numbers['dyo'])
-print(greek_numbers['tria'])
-print(greek_numbers['lalallaa'])
+treasure = {}
+setmetatable(treasure, mt)
+treasure.gold = 50
+print(treasure.gold)
+print(treasure.silver)
+treasure.gold = 100
