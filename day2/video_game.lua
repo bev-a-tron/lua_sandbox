82d69ab3ed4da89311544a259af7eb5d13a41206
Beverly Lau <beverly.a.lau@gmail.com>
2017-05-15 10:59:50 -0700

    Prototypes

--- day2/video_game.lua
+++ day2/video_game.lua
@@ -17,3 +17,59 @@ print(clone.health)
 dietrich.take_hit(dietrich)
 print(dietrich.health)
 print(clone.health)
+
+Villain = {
+	health = 100,
+	new = function(self, name)
+		local obj = {
+			name = name,
+			health = self.health
+		}
+		setmetatable(obj, self)
+		self.__index = self
+		return obj
+	end,
+	take_hit = function(self)
+		self.health = self.health - 10
+	end
+}
+
+dietrich = Villain.new(Villain, "Dietrich")
+
+Villain.take_hit(dietrich)
+dietrich.take_hit(dietrich)
+
+SuperVillain = Villain.new(Villain)
+function SuperVillain.take_hit(self)
+	self.health = self.health - 5
+end
+
+
+toht = SuperVillain.new(SuperVillain, 'Toht')
+print(toht.health)
+
+toht.take_hit(toht)
+print(toht.health)
+
+Villain = { health = 100 }
+function Villain:new(name)
+		local obj = {
+			name = name,
+			health = self.health
+		}
+		setmetatable(obj, self)
+		self.__index = self
+		return obj
+end
+
+function Villain:take_hit()
+	self.health = self.health - 10
+end
+
+dietrich = Villain:new('Dietrich')
+dietrich:take_hit()
+print(dietrich.health)
+
+toht = SuperVillain:new('Toht')
+toht:take_hit()
+print(toht.health)
